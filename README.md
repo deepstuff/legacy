54bfe92bd4b71996e51069d9e1d1d38614150951
DallanQ <dallan@quass.org>
2017-03-17 08:50:06 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Legacy vectors
+==============
+
+Scripts to run neural nets over
+
+* First, install tensorflow: https://www.tensorflow.org/install/install_mac
+ (when you follow the instructions to run virtualenv to create your virtual environtment,
+ add --python=python3.6 or something to get python3: `virtualenv --python=python3.6 tensorflow`)
+* Second, download legacy-vectors.csv and put it in the "data" directory
+* Third, run `pip3 install requirements.txt`
+* Finally, cd into the legacy/legacy directory and run `python3 single-layer.py`
+ (the warnings are really unfortunate but it appears that you can ignore them.
+ I didn't spend too much time trying to get rid of them since going forward I'm expecting we'll
+ be coding directly to tensorflow core and not using tf.contrib.learn)
+
+Running with 100,000 iterations should result in an accuracy (% examples labeled correctly) of .973.
+This took 10 hours on my mac.
---
+++ legacy/single-layer.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# load dataset
+dataset = tf.contrib.learn.datasets.base.load_csv_without_header(filename='../data/legacy-vectors.csv',
+                                                                 target_dtype=np.float32,
+                                                                 features_dtype=np.float32)
+
+# split in to train and test sets
+train_data, test_data, train_target, test_target = \
+    train_test_split(dataset.data, dataset.target, test_size=0.33, stratify=dataset.target, random_state=1)
+
+# construct fake fNNN feature names - we have 585 real-valued columns (as opposed to sparse columns)
+feature_columns = [tf.contrib.layers.real_valued_column('f'+str(i)) for i in range(len(dataset.data[0]))]
+
+# create a dictionary of generic fNNN feature names -> 1d array of values for that feature
+# I don't know why tf.contrib.learn wants the data in this format
+def input_fn(data, target):
+    feature_cols = {'f'+str(i): tf.constant(data[:,i]) for i in range(len(data[0]))}
+    label = tf.constant(target)
+    return feature_cols, label
+
+def train_input_fn():
+    return input_fn(train_data, train_target)
+
+def test_input_fn():
+    return input_fn(test_data, test_target)
+
+# linear classifier
+classifier = tf.contrib.learn.LinearClassifier(feature_columns=feature_columns, model_dir='../models/testsingle-layer-model')
+
+# train classifier - ignore the warnings
+classifier.fit(input_fn=train_input_fn, steps=10)
+
+# evaluate classifier on train data
+train_results = classifier.evaluate(input_fn=train_input_fn, steps=1)
+
+# evaluate classifier on test data
+test_results = classifier.evaluate(input_fn=test_input_fn, steps=1)
+
+print('train accuracy', train_results['accuracy'])
+print('test accuracy', test_results['accuracy'])
